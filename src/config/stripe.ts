b3dd14de229fef0e04162de13a7f6287e6af01e5
Hodari <duhodar1@gmail.com>
2026-06-01 11:20:01 +0300

    fix: update Stripe apiVersion to 2026-05-27.dahlia

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
-
+  
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-05-27.dahlia",
+    apiVersion: "2026-05-27.dahlia",
 });
 
 export default stripe;
